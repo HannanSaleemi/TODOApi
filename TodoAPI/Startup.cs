a88636e85854548198f806a9f6a4c45bfa738879
Hannan Saleemi <hannan@octopuslabs.com>
2019-03-01 21:08:05 +0000

    Registerd the DB Context

--- TodoAPI/Startup.cs
+++ TodoAPI/Startup.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+
+// Register the Database Context
+// Services like DB Context must be registered with the dependency injection (DI) container.
+// The container provides the service to controllers
+using Microsoft.EntityFrameworkCore;
+using TodoAPI.Models;
 
 namespace TodoAPI
 {
@@ -25,6 +24,8 @@ namespace TodoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
